e0131972123f909fc364ef958379c7a2ad6250ee
garmayev <garmayev@yandex.ru>
2025-03-28 19:52:24 +0800

    Fix calendar table

--- views/site/calendar.php
+++ views/site/calendar.php
@@ -5,7 +5,7 @@
  */
 
 $this->title = \Yii::t('app', 'Calendar');
-$this->registerJsFile("/buildCalendar/js/main.3cc3a96d.js", ['depends' => [\yii\web\JqueryAsset::class]]);
+$this->registerJsFile("/buildCalendar/js/main.ce14c287.js", ['depends' => [\yii\web\JqueryAsset::class]]);
 $this->registerJsFile("/buildCalendar/js/453.57e5436f.chunk.js", ['depends' => [\yii\web\JqueryAsset::class]]);
 $this->registerCssFile("/buildCalendar/css/main.2c330e1e.css");
 echo \yii\helpers\Html::tag("div", "", ["class" => "coworker-form"]);
