0afd4a730e0cb5319efcddff11b04736fc918d79
Garmayev <garmayev@yandex.ru>
2025-06-16 09:08:40 +0000

    Fixes

--- modules/api/controllers/CategoryController.php
+++ modules/api/controllers/CategoryController.php
@@ -3,10 +3,75 @@
 namespace app\modules\api\controllers;
 
 use app\models\Category;
-use yii\rest\Controller;
+use yii\rest\ActiveController;
 
-class CategoryController extends Controller
+class CategoryController extends ActiveController
 {
+    public $modelClass = Category::class;
+
+    public function behaviors()
+    {
+        return [
+            'corsFilter' => [
+                'class' => \yii\filters\Cors::class,
+                'cors' => [
+                    'Origin' => ['*'],
+                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'PREFLIGHT'],
+                    'Access-Control-Request-Headers' => ['*'],
+                    'Access-Control-Allow-Credentials' => false,
+                    'Access-Control-Max-Age' => 86400,
+                    'Access-Control-Allow-Origin' => ['*'],
+                ],
+            ],
+            'access' => [
+                'class' => \yii\filters\AccessControl::class,
+                'rules' => [
+                    // Guests
+                    [ 'allow' => true, 'roles' => ['?'], 'actions' => [] ],
+                    // Users
+                    [ 'allow' => true, 'roles' => ['@'], 'actions' => ['index', 'check', 'list', 'view', 'create', 'suitableOrders', 'calendar-month'] ],
+                ],
+            ],
+            'authenticator' => [
+                'class' => \yii\filters\auth\HttpBearerAuth::class,
+                'except' => ['OPTIONS', 'PREFLIGHT', 'HEAD', 'index']
+            ],
+        ];
+    }
+
+    protected function verbs()
+    {
+        return [
+            'index' => ['GET', 'OPTIONS'],
+            'view' => ['GET', 'OPTIONS'],
+            'create' => ['POST', 'OPTIONS'],
+            'update' => ['POST', 'PUT', 'OPTIONS'],
+            'delete' => ['DELETE', 'OPTIONS'],
+            'check' => ['POST', 'OPTIONS'],
+            'login' => ['POST', 'OPTIONS'],
+        ];
+    }
+
+    public function beforeAction($action)
+    {
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        return parent::beforeAction($action);
+    }
+
+    public function actions()
+    {
+        $actions = parent::actions();
+        unset($actions['index']);
+        return $actions;
+    }
+
+    public function prepareDataProvider()
+    {
+        return new \yii\data\ActiveDataProvider([
+            'query' => \app\models\Coworker::find()->where(['created_by' => \Yii::$app->user->getId()])
+        ]);
+    }
+
     public function actionIndex($type = 1)
     {
         return ['results' => Category::find()->where(['type' => $type])->all()];
