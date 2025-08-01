10abc5a0020342d9cfe8d536c9314d3e29ac7161
garmayev <garmayev@yandex.ru>
2025-08-01 15:06:06 +0800

    Fix telegram commands

--- modules/api/commands/command/MyCommand.php
+++ modules/api/commands/command/MyCommand.php
@@ -9,16 +9,18 @@ class MyCommand extends BaseCommand implements CommandInterface
 
     public function handle($telegram, $args)
     {
+        $telegram = \Yii::$app->telegram;
+        $message = $telegram->input->message;
         $user = \app\models\User::find()
             ->joinWith('profile')
-            ->where(['profile.chat_id' => $this->message->from->id])
+            ->where(['profile.chat_id' => $message->from->id])
             ->one();
         $keyboard = [];
         foreach ($user->orders as $order) {
             $keyboard[] = [['text' => \Yii::t('app', 'Order #{id}', ['id' => $order->id]), 'callback_data' => '/view_order id=' . $order->id]];
         }
         $telegram->sendMessage([
-            'chat_id' => $this->message->from->id,
+            'chat_id' => $message->from->id,
             'text' => (empty($keyboard)) ? \Yii::t('telegram', 'command_empty') : \Yii::t('telegram', 'command_order_list'),
             'reply_markup' => json_encode([
                 'inline_keyboard' => $keyboard
