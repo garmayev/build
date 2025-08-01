f7f5a8924ecd517bba866a59176ea79c804d4f0d
garmayev <garmayev@yandex.ru>
2025-08-01 10:54:47 +0800

    Fix calendar

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -29,7 +29,7 @@ class AppAsset extends AssetBundle
     public $js = [
         'https://cdn.jsdelivr.net/npm/jquery.maskedinput@1.4.1/src/jquery.maskedinput.min.js',
         '/react/js/453.abd1f321.chunk.js',
-        '/react/js/main.30adfbd1.js',
+        '/react/js/main.15f1c3ba.js',
     ];
     public $depends = [
         YiiAsset::class,
