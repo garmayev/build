7d5d02720993e62d1918f0e52078ac3f2226e0ec
garmayev <garmayev@yandex.ru>
2025-09-04 14:22:08 +0800

    fix photo_id

--- modules/api/commands/PhotoHandler.php
+++ modules/api/commands/PhotoHandler.php
@@ -10,8 +10,8 @@ class PhotoHandler extends command\BaseCommand implements CommandInterface
     public function handle($telegram, $args)
     {
         $message = $telegram->input->message;
-        $photos = $message->photos;
-        foreach ($photos as $photo) {
+        foreach ($message->photo as $photo) {
+            \Yii::error("https://api.telegram.org/file/bot{$telegram->botToken}/photo/{$photo->photo_id}/");
             \Yii::error($photo);
         }
     }
